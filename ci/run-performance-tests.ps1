4ff48818ed401d08298c7608aac3a105a26b06e8
Alexander Popel <alexander.popel@postindustria.com>
2025-01-17 15:23:40 +0200

    ci: fix typo in perf test

--- ci/run-performance-tests.ps1
+++ ci/run-performance-tests.ps1
@@ -5,4 +5,4 @@ param (
     [string]$Branch = "main"
 )
 
-./go/run-performance-tests.ps1 @PSBoundParameters -ExamplesRepo device-detection-examples-go -Example ./device-detection-exmples-go/dd/performance/performance.go
+./go/run-performance-tests.ps1 @PSBoundParameters -ExamplesRepo device-detection-examples-go -Example ./dd/performance/performance.go
