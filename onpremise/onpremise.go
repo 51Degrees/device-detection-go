77d6e4304a91457c938dc20fa98c52a4deb8f616
Marycka9 <marycka9@gmail.com>
2025-07-02 18:35:49 +0300

    Refactor: Remove unused code and consolidate file-related logic in onpremise engine

--- onpremise/onpremise.go
+++ onpremise/onpremise.go
@@ -42,28 +42,6 @@ const (
 	defaultDataFileUrl = "https://distributor.51degrees.com/api/v2/download?Type=HashV41&Download=True&Product=V4TAC"
 )
 
-// run starts the engine
-func (e *Engine) run() error {
-	e.recoverEngine()
-
-	go e.reloadFileEvent()
-
-	if err := e.processFileExternallyChanged(); err != nil {
-		return err
-	}
-
-	if err := e.validateAndAppendUrlParams(); err != nil {
-		return err
-	}
-
-	if e.IsAutoUpdateEnabled() {
-		e.SetFilePullerStarted(true)
-		go e.ScheduleFilePulling(e.stopCh, e.reloadFileEvents)
-	}
-
-	return nil
-}
-
 // New creates an instance of the on-premise device detection engine.  WithDataFile must be provided
 // to specify the path to the data file, otherwise initialization will fail
 func New(opts ...EngineOptions) (*Engine, error) {
@@ -118,74 +96,35 @@ func New(opts ...EngineOptions) (*Engine, error) {
 	return engine, nil
 }
 
-// recoverEngine handles recovery from a panic during file pulling, logs the error, and restarts the file pulling process if needed.
-func (e *Engine) recoverEngine() {
-	// recover from panic
-	// if panic occurs, we will log the error and restart the file pulling
-	defer func() {
-		if r := recover(); r != nil {
-			e.logger.Printf("error occurred when pulling data: %v", r)
-			if !e.isStopped {
-				go e.ScheduleFilePulling(e.stopCh, e.reloadFileEvents)
-			}
-		}
-	}()
-}
-
-// reloadFileEvent listens for file change events and processes them, handling external file modifications if detected.
-func (e *Engine) reloadFileEvent() {
-	for range e.reloadFileEvents {
-		if err := e.processFileExternallyChanged(); err != nil {
-			return
-		}
-	}
-}
-
-// Evidence struct encapsulates the evidence provided as input to the Process function
-// Evidence is usually an HTTP header, thus Prefix would be dd.HttpHeaderString,
-// but can also be a query param (dd.HttpEvidenceQuery) or a cookie (dd.HttpEvidenceCookie)
-type Evidence struct {
-	Prefix dd.EvidencePrefix
-	Key    string
-	Value  string
-}
-
-// Process detects the device from the provided evidence list
-// returns the dd.ResultsHash object from which various device properties
-// are retrieved
-func (e *Engine) Process(evidenceList []Evidence) (*dd.ResultsHash, error) {
-	evidenceHash, err := mapEvidence(evidenceList)
-	if err != nil {
-		return nil, err
-	}
-	defer evidenceHash.Free()
-
-	results := dd.NewResultsHash(e.manager, uint32(evidenceHash.Count()), 0)
-	err = results.MatchEvidence(evidenceHash)
-	if err != nil {
-		return nil, fmt.Errorf("failed to match evidence: %w", err)
+// handleFileExternallyChanged handles events where a file has been externally modified, ensuring proper processing and logging.
+func (e *Engine) handleFileExternallyChanged() {
+	if err := e.processFileExternallyChanged(); err != nil {
+		e.logger.Printf("failed to handle file externally changed: %v", err)
 	}
 
-	return results, nil
+	e.IncreaseFileExternallyChangedCount()
 }
 
-// mapEvidence converts a list of Evidence into a dd.Evidence object for further processing, handling errors if any occur.
-func mapEvidence(evidenceList []Evidence) (*dd.Evidence, error) {
-	evidenceHash := dd.NewEvidenceHash(uint32(len(evidenceList)))
+// run starts the engine
+func (e *Engine) run() error {
+	e.recoverEngine()
 
-	for _, evidence := range evidenceList {
-		err := evidenceHash.Add(evidence.Prefix, evidence.Key, evidence.Value)
-		if err != nil {
-			return nil, fmt.Errorf("failed to add evidence: %w", err)
-		}
+	go e.reloadFileEvent()
+
+	if err := e.processFileExternallyChanged(); err != nil {
+		return err
 	}
 
-	return evidenceHash, nil
-}
+	if err := e.validateAndAppendUrlParams(); err != nil {
+		return err
+	}
 
-// Create a new raw ResultsHash object.
-func (e *Engine) NewResultsHash(evidenceCapacity uint32, overridesCapacity uint32) *dd.ResultsHash {
-	return dd.NewResultsHash(e.manager, evidenceCapacity, overridesCapacity)
+	if e.IsAutoUpdateEnabled() {
+		e.SetFilePullerStarted(true)
+		go e.ScheduleFilePulling(e.stopCh, e.reloadFileEvents)
+	}
+
+	return nil
 }
 
 // Stop has to be called to free all the resources of the engine
@@ -211,6 +150,7 @@ func (e *Engine) Stop() {
 
 	e.isStopped = true
 	close(e.stopCh)
+	close(e.reloadFileEvents)
 
 	if e.manager != nil {
 		e.manager.Free()
@@ -224,29 +164,46 @@ func (e *Engine) Stop() {
 	}
 }
 
-// GetHttpHeaderKeys returns all HTTP headers that can be used as evidence for device detection
-func (e *Engine) GetHttpHeaderKeys() []dd.EvidenceKey {
-	return e.manager.HttpHeaderKeys
+// recoverEngine handles recovery from a panic during file pulling, logs the error, and restarts the file pulling process if needed.
+func (e *Engine) recoverEngine() {
+	// recover from panic
+	// if panic occurs, we will log the error and restart the file pulling
+	defer func() {
+		if r := recover(); r != nil {
+			e.logger.Printf("error occurred when pulling data: %v", r)
+			if !e.isStopped {
+				go e.ScheduleFilePulling(e.stopCh, e.reloadFileEvents)
+			}
+		}
+	}()
 }
 
-// appendLicenceKey updates the data file URL by appending a license key to its query parameters and returns an error if it fails.
-func (e *Engine) appendLicenceKey() error {
-	urlParsed, err := url.Parse(e.GetDataFileUrl())
-	if err != nil {
-		return err
+// reloadFileEvent listens for file change events and processes them, handling external file modifications if detected.
+func (e *Engine) reloadFileEvent() {
+	for range e.reloadFileEvents {
+		if err := e.processFileExternallyChanged(); err != nil {
+			return
+		}
 	}
-	query := urlParsed.Query()
-	query.Set("LicenseKeys", e.licenseKey)
-	urlParsed.RawQuery = query.Encode()
+}
 
-	e.SetDataFileUrl(urlParsed.String())
+// validateAndAppendUrlParams validates and appends necessary URL parameters based on the engine's configuration and state.
+func (e *Engine) validateAndAppendUrlParams() error {
+	if e.isDefaultDataFileUrl() && !e.hasDefaultDistributorParams() && e.IsAutoUpdateEnabled() {
+		return common_go.ErrLicenseKeyRequired
+	}
+
+	if e.isDefaultDataFileUrl() && e.IsAutoUpdateEnabled() {
+		if err := e.appendLicenceKey(); err != nil {
+			return err
+		}
+
+		if err := e.appendProduct(); err != nil {
+			return err
+		}
+	}
 
 	return nil
-}
-
-// isDefaultDataFileUrl checks if the engine's data file URL matches the predefined default data file URL.
-func (e *Engine) isDefaultDataFileUrl() bool {
-	return e.GetDataFileUrl() == defaultDataFileUrl
 }
 
 // appendProduct appends a "Product" query parameter to the Engine's data file URL using the provided product value.
@@ -264,34 +221,14 @@ func (e *Engine) appendProduct() error {
 	return nil
 }
 
-// validateAndAppendUrlParams validates and appends necessary URL parameters based on the engine's configuration and state.
-func (e *Engine) validateAndAppendUrlParams() error {
-	if e.isDefaultDataFileUrl() && !e.hasDefaultDistributorParams() && e.IsAutoUpdateEnabled() {
-		return common_go.ErrLicenseKeyRequired
-	}
-
-	if e.isDefaultDataFileUrl() && e.IsAutoUpdateEnabled() {
-		err := e.appendLicenceKey()
-		if err != nil {
-			return err
-		}
-		err = e.appendProduct()
-		if err != nil {
-			return err
-		}
-	}
-
-	return nil
+// isDefaultDataFileUrl checks if the engine's data file URL matches the predefined default data file URL.
+func (e *Engine) isDefaultDataFileUrl() bool {
+	return e.GetDataFileUrl() == defaultDataFileUrl
 }
 
 // hasDefaultDistributorParams checks whether the engine has default distributor parameters set via the license key.
 func (e *Engine) hasDefaultDistributorParams() bool {
 	return len(e.licenseKey) > 0
-}
-
-// hasSomeDistributorParams checks if the engine contains distributor-related parameters such as licenseKey or product.
-func (e *Engine) hasSomeDistributorParams() bool {
-	return len(e.licenseKey) > 0 || len(e.product) > 0
 }
 
 // processFileExternallyChanged handles updates when a file is changed externally by reloading its content.
@@ -306,16 +243,6 @@ func (e *Engine) processFileExternallyChanged() error {
 	}
 
 	return nil
-}
-
-// handleFileExternallyChanged handles events where a file has been externally modified, ensuring proper processing and logging.
-func (e *Engine) handleFileExternallyChanged() {
-	err := e.processFileExternallyChanged()
-	if err != nil {
-		e.logger.Printf("failed to handle file externally changed: %v", err)
-	}
-
-	e.IncreaseFileExternallyChangedCount()
 }
 
 // this function will be called when the engine is started or the is new file available
@@ -338,7 +265,6 @@ func (e *Engine) reloadManager(filePath string) error {
 			e.config = dd.NewConfigHash(dd.Balanced)
 		}
 		err := dd.InitManagerFromFile(e.manager, *e.config, e.managerProperties, filePath)
-
 		if err != nil {
 			return fmt.Errorf("failed to init manager from file: %w", err)
 		}
@@ -371,4 +297,76 @@ func (e *Engine) reloadManager(filePath string) error {
 // getPublishedDate retrieves the published date of the data file used by the engine.
 func (e *Engine) getPublishedDate() time.Time {
 	return dd.GetPublishedDate(e.manager)
+}
+
+// Create a new raw ResultsHash object.
+func (e *Engine) NewResultsHash(evidenceCapacity uint32, overridesCapacity uint32) *dd.ResultsHash {
+	return dd.NewResultsHash(e.manager, evidenceCapacity, overridesCapacity)
+}
+
+// Process detects the device from the provided evidence list
+// returns the dd.ResultsHash object from which various device properties
+// are retrieved
+func (e *Engine) Process(evidenceList []Evidence) (*dd.ResultsHash, error) {
+	evidenceHash, err := mapEvidence(evidenceList)
+	if err != nil {
+		return nil, err
+	}
+	defer evidenceHash.Free()
+
+	results := dd.NewResultsHash(e.manager, uint32(evidenceHash.Count()), 0)
+	err = results.MatchEvidence(evidenceHash)
+	if err != nil {
+		return nil, fmt.Errorf("failed to match evidence: %w", err)
+	}
+
+	return results, nil
+}
+
+// GetHttpHeaderKeys returns all HTTP headers that can be used as evidence for device detection
+func (e *Engine) GetHttpHeaderKeys() []dd.EvidenceKey {
+	return e.manager.HttpHeaderKeys
+}
+
+// appendLicenceKey updates the data file URL by appending a license key to its query parameters and returns an error if it fails.
+func (e *Engine) appendLicenceKey() error {
+	urlParsed, err := url.Parse(e.GetDataFileUrl())
+	if err != nil {
+		return err
+	}
+	query := urlParsed.Query()
+	query.Set("LicenseKeys", e.licenseKey)
+	urlParsed.RawQuery = query.Encode()
+
+	e.SetDataFileUrl(urlParsed.String())
+
+	return nil
+}
+
+// hasSomeDistributorParams checks if the engine contains distributor-related parameters such as licenseKey or product.
+func (e *Engine) hasSomeDistributorParams() bool {
+	return len(e.licenseKey) > 0 || len(e.product) > 0
+}
+
+// Evidence struct encapsulates the evidence provided as input to the Process function
+// Evidence is usually an HTTP header, thus Prefix would be dd.HttpHeaderString,
+// but can also be a query param (dd.HttpEvidenceQuery) or a cookie (dd.HttpEvidenceCookie)
+type Evidence struct {
+	Prefix dd.EvidencePrefix
+	Key    string
+	Value  string
+}
+
+// mapEvidence converts a list of Evidence into a dd.Evidence object for further processing, handling errors if any occur.
+func mapEvidence(evidenceList []Evidence) (*dd.Evidence, error) {
+	evidenceHash := dd.NewEvidenceHash(uint32(len(evidenceList)))
+
+	for _, evidence := range evidenceList {
+		err := evidenceHash.Add(evidence.Prefix, evidence.Key, evidence.Value)
+		if err != nil {
+			return nil, fmt.Errorf("failed to add evidence: %w", err)
+		}
+	}
+
+	return evidenceHash, nil
 }
