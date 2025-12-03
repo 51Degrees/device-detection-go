4e734410c064621be787d6ffbd091720bcf01ce9
Alexander Popel <74662418+pi-alexander-popel@users.noreply.github.com>
2025-12-03 04:42:31 +0200

    Merged Pull Request '#70 common-fetch-assets->main: ci: use common fetch-assets script'

--- ci/fetch-assets.ps1
+++ ci/fetch-assets.ps1
@@ -1,21 +1,8 @@
-param (
-    [Parameter(Mandatory=$true)]
-    [string]$RepoName
-)
 $ErrorActionPreference = "Stop"
 
-$assets = New-Item -ItemType Directory -Path assets -Force
-$assetsDestination = "$RepoName"
-$files = "51Degrees-LiteV4.1.hash", "20000 Evidence Records.yml"
+$assets = "51Degrees-LiteV4.1.hash", "20000 Evidence Records.yml"
 
-foreach ($file in $files) {
-    if (!(Test-Path $assets/$file)) {
-        Write-Output "Downloading $file"
-        Invoke-WebRequest -Uri "https://github.com/51Degrees/device-detection-data/raw/main/$file" -OutFile $assets/$file
-    } else {
-        Write-Output "'$file' exists, skipping download"
-    }
-
-    New-Item -ItemType SymbolicLink -Force -Target "$assets/$file" -Path "$assetsDestination/$file"
+./steps/fetch-assets.ps1 -Assets $assets
+foreach ($asset in $assets) {
+    New-Item -ItemType SymbolicLink -Force -Target "$PWD/assets/$asset" -Path "$PSScriptRoot/../$asset"
 }
-
